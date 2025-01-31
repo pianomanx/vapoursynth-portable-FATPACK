63712af4f50e704e7edf89d94aad2928b0e22b44
Yohan050 <yohanfr.tordjman@gmail.com>
2025-01-30 23:39:49 -0500

    Update _vs_plugin_check.bat
    Forget to change folder.

--- VapourSynth64Portable/_vs_plugin_check.bat
+++ VapourSynth64Portable/_vs_plugin_check.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-VapourSynth64\python.exe VapourSynth64\vs_plugin_check.py "%cd%\VapourSynth64\vapoursynth64\vs-plugins"
+VapourSynth64\python.exe VapourSynth64\vs_plugin_check.py "%cd%\VapourSynth64\vs-plugins"
 pause
